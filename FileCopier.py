46feb21035aacf2a620b1b73fa34d8b73f8cde8b
Chinmay Bhushan <bhushan.chinmay@gmail.com>
2020-06-08 16:30:48 +0530

    File Copier Script Modified

--- FileCopier.py
+++ FileCopier.py
@@ -8,29 +8,27 @@ import os
 import shutil
 from os import path
 
-DIRECTORY_NAME = 'C:\\Users\\chinbhus\\OneDrive - Qualcomm\\Desktop\\Progs\\Hello\\'  # Directory where file whose copies are to be made is pasted
-FILE_NAME = '../QCN/'  # Folder where these copies will be created
-adblist = 'adb.txt'
-
-os.chdir(
-    'C:\\Users\\chinbhus\\OneDrive - Qualcomm\\Desktop\\Progs\\Hello\\')  # changing directory where we have pasted our file whose copies will be created
-
+DIRECTORY_NAME = 'C:/'  # this variable contains directory name which stores file whose copies will be created
+FILE_NAME = './QCN/'  # name of the folder where copies will be created
+adblist = 'adb.txt'  # this variable contains txt file containing name which will be used for naming
 
 # We can either use the function 'getFileName()' or we can directly use 'QCN_FILE_NAME' and 'EXTENSION'
-# QCN_FILE_NAME = 'a'
+QCN_FILE = 'a'  # default name for file whose copies will be created
 EXTENSION = '.xqcn'  # Extension for file type whose copies will be created
-
 DEVICE_ABD_ID = []  # used for storing list of abd devices
 
 
-# Function will find the file whose copies will be created according to its 'EXTENSION'
+# Function gets the name of the file whose copies will be created with
 def getFileName():
+    global QCN_FILE
     for findFile in glob.glob('*' + EXTENSION):
-        global QCN_FILE_NAME, EXTENSIONS
-        QCN_FILE_NAME, EXTENSIONS = findFile.split('.')
+        QCN_FILE_NAME = ''
+        EXTENSIONS = ''
+        QCN_FILE, EXTENSIONS = findFile.split('.')[1:]
+    return QCN_FILE
 
 
-# Get list of devices using the 'abdlist' file
+# Get list of names that will be used for naming
 def GET_DEVICE_LIST(filename):
     with open(filename) as adb_list:
         for devices in adb_list:
@@ -41,12 +39,13 @@ def GET_DEVICE_LIST(filename):
 
 
 def main():
-    if path.exists(QCN_FILE + EXTENSION):
-        getFileName()
-        GET_DEVICE_LIST(adblist)
-        for d in range(len(DEVICE_ABD_ID)):
-            shutil.copyfile(QCN_FILE + EXTENSIONS, FILE_NAME + DEVICE_ABD_ID[
-                d] + EXTENSIONS)  # function copies files and renames it, both functions are done simultaneously
+    global QCN_FILE, EXTENSION
+    os.chdir(DIRECTORY_NAME)  # changing directory where we have pasted our file, whose copies will be created
+    getFileName()
+    QCN_FILE = GET_DEVICE_LIST(adblist)
+    for d in range(len(DEVICE_ABD_ID)):
+        if path.exists(QCN_FILE[d] + EXTENSION):
+            shutil.copyfile(QCN_FILE[d] + EXTENSION, FILE_NAME + DEVICE_ABD_ID[d] + EXTENSIONS)  # function copies files and renames the file, both the functions are performed simultaneously
 
 
 if __name__ == "__main__":
