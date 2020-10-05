dae161db43d430ef3d5f1e04668b65c29a472e85
Chinmay Bhushan <bhushan.chinmay@gmail.com>
2020-10-05 17:59:18 +0530

    Coin change problem

---
+++ coin_change_problem.py
@@ -0,0 +1,27 @@
+def getNumberOfWays(N, Coins): 
+
+	ways = [0] * (N + 1); 
+
+	ways[0] = 1; 
+
+	for i in range(len(Coins)): 
+
+		for j in range(len(ways)): 
+			if (Coins[i] <= j): 
+
+				ways[j] += ways[(int)(j - Coins[i])]; 
+
+	return ways[N]; 
+
+def printArray(coins): 
+	for i in coins: 
+		print(i); 
+
+if __name__ == '__main__': 
+	Coins = [1, 5, 10]; 
+
+	print("The Coins Array:"); 
+	printArray(Coins); 
+
+	print("Solution:",end=""); 
+	print(getNumberOfWays(6, Coins)); 
